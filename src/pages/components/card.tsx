35277fb586bb2b63f9dc7658c4476bedf46d5392
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2024-04-20 23:04:18 +0700

    feat: updated p2s1 logo

--- src/pages/components/card.tsx
+++ src/pages/components/card.tsx
@@ -7,15 +7,15 @@ const Cards = () => {
       <div className="card">
         <div className="card-image">
           <Image
-            src="./Logo P2S1.png"
+            src="./Documentation - 2024/Logo P2S1.png"
             alt="P2S1 Logo"
             layout="fill"
             unoptimized
           />
         </div>
         <div className="card-text">
-          <h1>P2S1 XXVI</h1>
-          <h1>2023</h1>
+          <h1>P2S1 XXVII</h1>
+          <h1>2024</h1>
           <p className="text-justify">
             The 26th P2S1 started on the first week of January and ended on the
             first week of March. A total of 48 competitions were held in this
@@ -28,7 +28,7 @@ const Cards = () => {
       <div className="card">
         <div className="card-image">
           <Image
-            src="./Logo P2S1.png"
+            src="./Documentation - 2023/Logo P2S1.png"
             alt="P2S1 Logo"
             layout="fill"
             unoptimized
